4a337b14a8cd19c2b43025397b1393565aa337c3
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2020-08-23 01:07:50 +0300

    remove unused fields

--- src/main/java/com/tele2/market/model/Settings.java
+++ src/main/java/com/tele2/market/model/Settings.java
@@ -8,10 +8,8 @@ public class Settings {
     private boolean sms;
     private boolean gigabyte;
     private boolean minute;
-    private boolean defaultCost;
 
     private Product  product;
-    private Long defaultCosts;
     private Integer startApplySettingsDay;
     private Double cost;
 }
