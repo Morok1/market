e137f5161b1ab14fc4439b3a60210eb61d4083b2
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2020-08-22 16:12:11 +0300

    init lot store

--- src/main/java/com/tele2/market/dao/LotDaoCustom.java
+++ src/main/java/com/tele2/market/dao/LotDaoCustom.java
@@ -1,11 +1,14 @@
 package com.tele2.market.dao;
 
 import com.tele2.market.model.Lot;
+import com.tele2.market.model.Type;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import static com.tele2.market.utils.Util.createId;
+
 @Component
 public class LotDaoCustom implements LotDao {
     static Queue<Lot> store =  new LinkedList<>();
@@ -18,5 +21,20 @@ public class LotDaoCustom implements LotDao {
     @Override
     public Queue<Lot> getAllLots() {
         return store;
+    }
+
+    public static final int LOT_NUMBER = 20;
+
+    static {
+        for (int i = 0; i < LOT_NUMBER; i++) {
+            initStore(i);
+        }
+    }
+
+    private static void initStore(int i) {
+        store.add(Lot.builder().type(Type.INTERNET)
+                .buyerId(createId())
+                .sellerId(createId()).id(Long.valueOf(i))
+                .build());
     }
 }
