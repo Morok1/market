46d78e1e8b0ef0859a0f53f0f2328d57eb889af3
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2020-08-22 16:03:08 +0300

    fix cors policy

--- src/main/java/com/tele2/market/controllers/LauncherController.java
+++ src/main/java/com/tele2/market/controllers/LauncherController.java
@@ -2,10 +2,12 @@ package com.tele2.market.controllers;
 
 import com.tele2.market.service.LauncherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class LauncherController {
     private final LauncherService service;
     @Autowired
